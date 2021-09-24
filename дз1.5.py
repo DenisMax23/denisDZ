b5fb017caa51498099def418b76e2a22f9f514ad
DenisMax23 <91289607+DenisMax23@users.noreply.github.com>
2021-09-24 22:58:24 +0300

    Add files via upload

---
+++ дз1.5.py
@@ -0,0 +1,14 @@
+a=float(input())
+b=float(input())
+if a>0 and b>0:
+    print('1 четверть')
+elif a<0 and b>0:
+    print('2 четверть')
+elif a<0 and b<0:
+    print('3 четверть')    
+elif a>0 and b<0:
+    print('4 четверть')    
+if a==0 and b==0:
+    print('начало координат')    
+    
+    
